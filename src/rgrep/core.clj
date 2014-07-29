5f11204225b8ee8f1da1a3c0c6b483c139533a6f
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-28 23:26:23 -0500

    Add command line argument processing

--- src/rgrep/core.clj
+++ src/rgrep/core.clj
@@ -1,6 +1,15 @@
-(ns rgrep.core)
+(ns rgrep.core
+  (:require [clojure.tools.cli :refer [cli]])
+  (:gen-class))
 
 (defn help
   "Generic help about RGrep"
   []
-  (println x "A Grep clone written in Clojure"))
+  (println "A Grep clone written in Clojure"))
+
+(defn -main [& args]
+  (let [[opts args banner] (cli args
+                             ["-h" "--help" "Print this help"
+                              :default false :flag true])]
+    (when (:help opts)
+      (println banner))))
