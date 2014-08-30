1fd57b316273c6c14c1857c5313eb4c6452774ff
ambidextrousTx <ravisinha@my.unt.edu>
2014-08-29 22:51:24 -0500

    Add case-insensitive match

--- src/rgrep/matching.clj
+++ src/rgrep/matching.clj
@@ -1,4 +1,5 @@
 (ns rgrep.core)
+(require 'clojure.string)
 
 (defn match [x y]
   "Looks for a direct match between two strings"
@@ -7,3 +8,7 @@
 (defn nomatch [x y]
   "Looks for direct mismatch"
   (not(= x y)))
+
+(defn match-case-insensitive [x y]
+  "Match case-insensitively"
+  (= (clojure.string/lower-case x) (clojure.string/lower-case y)))
